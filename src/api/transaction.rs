f3253f4730096c4444801de88f34243335761b68
Pakorn Nathong <pakorn.nth@hotmail.com>
2024-02-15 15:38:30 +0700

    feat: add tx detail

--- src/api/transaction.rs
+++ src/api/transaction.rs
@@ -14,7 +14,7 @@ use crate::{
 
 pub fn routes() -> Router<()> {
     Router::new()
-        .route("/:chain-id/:hash", get(tx_hash))
+        .route("/:hash", get(tx_hash))
         .route_layer(middleware::from_fn_with_state(
             STATE.clone(),
             LongAlwaysCacheMiddleware::<false>::handler,
@@ -23,33 +23,40 @@ pub fn routes() -> Router<()> {
 }
 
 pub async fn tx_hash(
-    Path((chain_id, hash)): Path<(String, String)>,
+    Path(hash): Path<String>,
     State(state): State<AppState>,
 ) -> Result<Json<Value>, AppError> {
     let postgres = state.postgres_pool.get().await?;
-    let chain_id = chain_id.parse::<i64>()?;
 
     let results = postgres
         .query(
-            "SELECT from_address, to_address, transaction_hash, transaction_index, block_number, value, input, gas_used_total, error, function_signature FROM transactions WHERE chain_id = $1 AND transaction_hash = $2 LIMIT 1",
-            &[&chain_id, &hash],
+            "SELECT transactions.chain_id, from_address, to_address, transaction_hash, transaction_index, block_number, blocks.timestamp AS block_timestamp, value, input, gas_used_total, gas_used_first_degree, error, function_signature, sig_names.name AS function_name, ec_pairing_count, ec_recover_count, ec_recover_addresses, ec_pairing_input_sizes, closest_address FROM transactions LEFT JOIN sig_names ON transactions.function_signature = sig_names.sig LEFT JOIN blocks ON blocks.chain_id = transactions.chain_id AND blocks.number = transactions.block_number WHERE transaction_hash = $1 LIMIT 1",
+            &[&hash],
         )
         .await?;
     let result = results.get(0).ok_or_else(AppError::not_found)?;
 
     Ok(Json(json!({
-        "chain_id": chain_id,
         "data": {
+            "chain_id": result.try_get::<_, i64>("chain_id")?,
             "from_address": result.try_get::<_, String>("from_address")?,
             "to_address": result.try_get::<_, String>("to_address")?,
             "transaction_hash": result.try_get::<_, String>("transaction_hash")?,
             "transaction_index": result.try_get::<_, i32>("transaction_index")?,
             "block_number": result.try_get::<_, i64>("block_number")?,
+            "block_timestamp": result.try_get::<_, i64>("block_timestamp")?,
             "value": from_str::<Number>(&result.try_get::<_, String>("value")?)?,
             "input": result.try_get::<_, String>("input")?,
             "gas_used_total": result.try_get::<_, i64>("gas_used_total")?,
+            "gas_used_first_degree": result.try_get::<_, i64>("gas_used_first_degree")?,
             "error": result.try_get::<_, Option<String>>("error")?,
             "function_signature": result.try_get::<_, Option<String>>("function_signature")?,
+            "function_name": result.try_get::<_, Option<String>>("function_name")?,
+            "ec_pairing_count": result.try_get::<_, i16>("ec_pairing_count")?,
+            "ec_recover_count": result.try_get::<_, i16>("ec_recover_count")?,
+            "ec_recover_addresses": result.try_get::<_, Vec<String>>("ec_recover_addresses")?,
+            "ec_pairing_input_sizes": result.try_get::<_, Vec<i32>>("ec_pairing_input_sizes")?,
+            "closest_address": result.try_get::<_, Vec<String>>("closest_address")?,
         }
     })))
 }
